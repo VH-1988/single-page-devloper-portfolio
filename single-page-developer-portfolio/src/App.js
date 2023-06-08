9e62156ec59480737674925d7188e4269f141898
Vuyolwethu Halam <vhalam@gmail.com>
2023-06-08 14:06:41 +0200

    update 01

--- single-page-developer-portfolio/src/App.js
+++ single-page-developer-portfolio/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import './index.css';
 import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='navbar'>
+        <h1>Vuyolwethu Halam</h1>
+        <Link>github</Link>
+        <Link>linkedin</Link>
+        <Link>tweeter</Link>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
+
